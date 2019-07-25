065f86628a1515b4c42a4c492e07494f7ff67b24
Levi Miller <paullevimiller@gmail.com>
2019-07-25 11:04:38 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  @all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    sorted_object_word = @word.split("").sort
+    word_array.select { |array_word| sorted_object_word == array_word.split("").sort}
+  end
+
+end
